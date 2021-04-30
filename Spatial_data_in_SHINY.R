8fa454b7692503a648ab11b3e0a5e9bcf6937ec4
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-04-30 10:53:55 +0100

    Create map of newcastle

--- Spatial_data_in_SHINY.R
+++ Spatial_data_in_SHINY.R
@@ -1,3 +1,9 @@
 library(leaflet)
 library(leafem)
 library(mapview)
+
+# Create the map
+my_map <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lng=-1.6178, lat=54.9783, popup="World's most important city!")
+my_map #display the map
