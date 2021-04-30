9b2f561781dbeadd0572f0f880ac163f15fa9948
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-04-30 11:06:40 +0100

    Adding more may layers and specifying opacity

--- Spatial_data_in_SHINY.R
+++ Spatial_data_in_SHINY.R
@@ -7,3 +7,14 @@ my_map <- leaflet() %>%
   addTiles() %>%  # Add default OpenStreetMap map tiles
   addMarkers(lng=-1.6178, lat=54.9783, popup="World's most important city!")
 my_map #display the map
+
+#try using different backdrops to show different map features
+#this shows satellite map overlaid with main roads and names of main roads
+leaflet() %>% 
+  addProviderTiles(providers$Esri.WorldImagery) %>% 
+  addProviderTiles(providers$Stamen.TonerLines,
+                   options = providerTileOptions(opacity = 0.5)) %>%
+  addProviderTiles(providers$Stamen.TonerLabels) %>% 
+  addMarkers(lng=-1.6178, lat=54.9783, popup="World's most important city!")
+
+  
